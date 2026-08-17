411c3ad83b8945601bff4366c34f4a5e13a46c6c
mbbrueckner <mikabenbrueckner@icloud.com>
2026-08-17 14:57:09 +0200

    chore (final project): add package exports

---
+++ final_project/src/sr/__init__.py
@@ -0,0 +1,4 @@
+from .expressions import OP_SETS, evaluate, to_string, to_sympy
+from .generator import Dataset, sample_dataset
+from .search import Candidate, pareto_search
+from .selection import extrapolation_folds, interior_folds, select
